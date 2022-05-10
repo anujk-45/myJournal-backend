753ca0834950e28ed6ea198669a17681dddb61bd
Anuj Kumar Jaimini <anujkumarjaimini025@gmail.com>
2022-05-10 15:21:58 +0530

    cors changes

--- src/index.js
+++ src/index.js
@@ -10,7 +10,9 @@ const app = express()
 const port = process.env.PORT || 5000
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin : 'https://akj-myjournal.netlify.app/'
+}));
 app.use('/blogs',blogRouter);
 app.use('/admin',adminRouter);
 
